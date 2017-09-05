2c0feeb9b328f89d54c1812582b567af9d6f3dee
Demetrio <dem3perez@gmail.com>
2017-09-05 15:58:31 -0400

    Build class for stack

--- DataStructures/DataStructures/StackObject.swift
+++ DataStructures/DataStructures/StackObject.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class StackObject: NSObject {
+class StackObject: NSObject
+{
+    private var deck:[String] = []
+    
+    private let noCards = "No more cards in deck"
+    func push(card:String)
+    {
+        deck.append(card)
+    }
+    
+    func peek()->String
+    {
+        if let card = deck.last
+        {
+            return card
+        }
+        else
+        {
+            return noCards
+        }
+    }
+    
+    func pop() ->String
+    {
+        if let card = deck.popLast()
+        {
+            return card
+        }
+        else
+        {
+            return noCards
+        }
+    }
 
 }
